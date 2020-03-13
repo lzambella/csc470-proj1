eeb642fb0a7b3dd6642323a354fd00e2331d67db
Luke Zambella <lukezambella@gmail.com>
2020-03-12 22:26:07 -0400

    Add basic function test for FSA.

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import state_machine
 class TestFSA(unittest.TestCase):
 
     def test_d_recognize(self):
+        """
+        Test the basic D_Recognize algorithm using the sheep languages discussed in class
+        the regex is defined as /^baa+!$/
+        """
         states = ["1", "2", "3", "4"]
         domain = ["b", "a", "!"]
         start_state = "1"
@@ -18,5 +22,43 @@ class TestFSA(unittest.TestCase):
         self.assertEqual("REJECT", FSA.D_Recognize("gary"))
         self.assertEqual("REJECT", FSA.D_Recognize("baaaaaaaa"))
         self.assertEqual("ACCEPT", FSA.D_Recognize("baa!"))
+
+    def test_d_recognize_b(self):
+        """
+        Tests the second d_recognize function by defining a machine that searches for the sequence "gary" in any string
+        """
+        states = ["1", "2", "3", "4", "5"]
+        domain = ["g", "a", "r", "y"]
+        start_state = "1"
+        accept_state = ["5"]
+        state_table = [("1","g","2"),
+                        ("1","a","1"),
+                        ("1","r","1"),
+                        ("1","y","1"),
+                        ("2","a","3"),
+                        ("2","g","1"),
+                        ("2","r","1"),
+                        ("2","y","1"),
+                        ("3","r","4"),
+                        ("3","g","1"),
+                        ("3","a","1"),
+                        ("3","y","1"),
+                        ("4","y","5"),
+                        ("4","g","1"),
+                        ("4","a","1"),
+                        ("4","r","1"),
+                        ("5","g","NULL"),
+                        ("5","a","NULL"),
+                        ("5","r","NULL"),
+                        ("5","y","NULL"),]
+        FSA = state_machine.Finite_State_Automata(states,domain,start_state,accept_state,state_table)
+
+        self.assertEqual("ACCEPT", FSA.D_Recognize_b("baaaa baaa gary!"))
+        self.assertEqual("REJECT", FSA.D_Recognize_b("lary"))
+        self.assertEqual("ACCEPT", FSA.D_Recognize_b("gar gary lary"))
+        self.assertEqual("REJECT", FSA.D_Recognize_b("garrrrrry"))
+        self.assertEqual("REJECT", FSA.D_Recognize_b("carlin"))
+        self.assertEqual("ACCEPT", FSA.D_Recognize_b("asdf&*YR@#UgaryJFES(*FY"))
+        self.assertEqual("REJECT", FSA.D_Recognize_b("Gary"))
 
 unittest.main()
